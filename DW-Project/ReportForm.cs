9ad2c284a3eb2446073f584e400b4b9b7b58dfc0
tsweeney256 <n00765395@osprey.unfcsd.unf.edu>
2015-04-15 15:58:00 -0400

    Initial support for creating reports

--- DW-Project/ReportForm.cs
+++ DW-Project/ReportForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace DW_Project
 {
     public partial class ReportForm : Form
     {
+
         public ReportForm()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void ExecuteSQLButtonClick(object sender, EventArgs e)
+        {
+            var connection = Factory.getNewDBConnection();
+
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("w/e");
+            }
+            finally
+            {
+                connection.Close();
+            }
+            try
+            {
+                connection.Open();
+                var command = new SqlCommand(SQLTextBox.Text, connection);
+                var reader = command.ExecuteReader();
+                try
+                {
+                    var readerRows = new ArrayList();
+                    String[] colsNames = null;
+                    if (reader.Read())
+                    {
+                        colsNames = new String[reader.FieldCount];
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            colsNames[i] = reader.GetName(i);
+                        }
+                    }
+
+                    do
+                    {
+                        var arr = new Object[reader.FieldCount];
+                        reader.GetValues(arr);
+                        readerRows.Add(arr);
+                    } while (reader.Read());
+                    var resultForm = new ResultForm(readerRows, colsNames);
+                    resultForm.Show();
+                }
+                catch (Exception e1)
+                {
+                    MessageBox.Show(e1.Message);
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show(e2.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
---
+++ DW-Project/ResultForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DW_Project
+{
+    public partial class ResultForm : Form
+    {
+        public ResultForm(ArrayList rows, String[] colNames)
+        {
+            InitializeComponent();
+            SQLGridView.ColumnCount = colNames.Length;
+            for (int i = 0; i < colNames.Length; i++)
+            {
+                SQLGridView.Columns[i].Name = colNames[i];
+            }
+            for (int i = 0; i < rows.Count; i++)
+            {
+                SQLGridView.Rows.Add((Object[])rows[i]);
+            }
+            if (colNames.Length == 2)
+            {
+                BarChart.Series.Clear();
+                BarChart.Series.Add("Series");
+                for (int i = 0; i < rows.Count; i++)
+                {
+                    BarChart.Series[0].Points.AddXY(((Object[])rows[i])[1], ((Object[])rows[i])[0]);
+                }
+            }
+        }
+    }
+}
