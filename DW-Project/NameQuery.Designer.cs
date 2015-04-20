1beb9bab087342455b3a8e3e42594c49a7ce40a2
tsweeney256 <n00765395@osprey.unfcsd.unf.edu>
2015-04-20 19:53:00 -0400

    forgot to add some files in those last commits

---
+++ DW-Project/NameQuery.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace DW_Project
+{
+    partial class NameQuery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OKButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.TheCancelButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(168, 73);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(100, 23);
+            this.OKButton.TabIndex = 0;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(28, 38);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(240, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // TheCancelButton
+            // 
+            this.TheCancelButton.Location = new System.Drawing.Point(28, 73);
+            this.TheCancelButton.Name = "TheCancelButton";
+            this.TheCancelButton.Size = new System.Drawing.Size(100, 23);
+            this.TheCancelButton.TabIndex = 2;
+            this.TheCancelButton.Text = "Cancel";
+            this.TheCancelButton.UseVisualStyleBackColor = true;
+            this.TheCancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter in name for new query.";
+            // 
+            // NameQuery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 102);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TheCancelButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.OKButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(298, 140);
+            this.MinimumSize = new System.Drawing.Size(298, 140);
+            this.Name = "NameQuery";
+            this.Text = "New Query";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button TheCancelButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DW-Project/NameQuery.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DW_Project
+{
+    public partial class NameQuery : Form
+    {
+        public string name { get; set; }
+        public NameQuery()
+        {
+            InitializeComponent();
+        }
+
+        public DialogResult ShowDialog(out string result)
+        {
+            DialogResult dialogResult = base.ShowDialog();
+
+            result = name;
+            return dialogResult;
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(nameTextBox.Text))
+            {
+                MessageBox.Show("Error. Name field is blank");
+            }
+            else
+            {
+                name = nameTextBox.Text;
+                this.Close();
+            }
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            name = "";
+            this.Close();
+        }
+    }
+}
